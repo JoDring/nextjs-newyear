adcd1094e5fc44c2e6992aff5bcbee772a634e3d
zhujing <yangzhujing@liyi99.com>
2021-12-28 11:10:13 +0800

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-    plugins: [
-        'tailwindcss',
-        'postcss-flexbugs-fixes',
-        [
-            'postcss-preset-env',
-            {
-                autoprefixer: {
-                    flexbox: 'no-2009'
-                },
-                stage: 3,
-                features: {
-                    'custom-properties': false
-                }
-            }
-        ]
-    ]
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
